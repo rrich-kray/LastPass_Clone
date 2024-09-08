43a74f1c71e36671f0f965479a777d6815bc7391
rrich-kray <rrich.kray.93@gmail.com>
2024-09-08 13:08:44 -0700

    Address Creation now works

--- lastpass_clone.client/src/Components/Addresses/AddressCreationUpdateForm/AddressCreationUpdateForm.tsx
+++ lastpass_clone.client/src/Components/Addresses/AddressCreationUpdateForm/AddressCreationUpdateForm.tsx
@@ -3,35 +3,37 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Category from "../../../Types/Category";
 import Address from "../../../Types/Address";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 
 const AddressCreationUpdateForm = ({ baseUrl, updateToggle, addressData }: { baseUrl: string, updateToggle: boolean, addressData: Address }) => {
     const [categories, setCategories] = useState<Category[]>();
     const [currentCategoryId, setCurrentCategoryId] = useState<number>();
     const [formState, setFormState] = useState({
-        addressName: "",
-        title: "",
-        firstName: "",
-        middleName: "",
-        lastName: "",
-        userName: "",
-        gender: "",
-        birthday: "",
-        company: "",
-        address1: "",
-        address2: "",
-        address3: "",
-        city: "",
-        county: "",
-        state: "",
-        zipCode: "",
-        country: "",
-        emailAddress: "",
-        phoneNumber: "",
-        eveningPhone: "",
-        mobilePhone: "",
-        fax: "",
-        notes: ""
+        AddressName: "",
+        Title: "",
+        FirstName: "",
+        MiddleName: "",
+        LastName: "",
+        UserName: "",
+        Gender: "",
+        Birthday: new Date('December 17, 1995 03:24:00'),
+        Company: "",
+        Address1: "",
+        Address2: "",
+        Address3: "",
+        City: "",
+        County: "",
+        State: "",
+        ZipCode: 0,
+        Country: "",
+        EmailAddress: "",
+        PhoneNumber: 0,
+        EveningPhone: 0,
+        MobilePhone: 0,
+        Fax: 0,
+        Notes: ""
     })
 
     useEffect(() => {
@@ -59,67 +61,67 @@ const AddressCreationUpdateForm = ({ baseUrl, updateToggle, addressData }: { bas
 
     const handleFormSubmit = async (e) => {
         e.preventDefault();
-        const address: Address =
-        {
-            categoryId: currentCategoryId!,
-            addressName: formState.addressName,
-            title: formState.title,
-            firstName: formState.firstName,
-            middleName: formState.middleName,
-            lastName: formState.lastName,
-            userName: formState.userName,
-            gender:formState.gender,
-            birthday: formState.birthday,
-            company: formState.company,
-            address1: formState.address1,
-            address2: formState.address2,
-            address3: formState.address3,
-            city: formState.city,
-            county: formState.county,
-            state: formState.state,
-            zipCode: formState.zipCode,
-            country: formState.country,
-            emailAddress: formState.emailAddress,
-            phoneNumber: formState.phoneNumber,
-            eveningPhone: formState.eveningPhone,
-            mobilePhone: formState.mobilePhone,
-            fax: formState.fax,
-            notes: formState.notes
-        };
         const headers = {
             'Content-Type': 'application/json'
         }
         return await
             !updateToggle
-            ? axios.post(`${baseUrl}/CreatePassword`, address, { headers: headers })
+            ? axios.post(
+                `${baseUrl}/CreateAddress`, 
+                {
+                    categoryId: currentCategoryId!,
+                    addressName: formState.AddressName,
+                    title: formState.Title,
+                    firstName: formState.FirstName,
+                    middleName: formState.MiddleName,
+                    lastName: formState.LastName,
+                    userName: formState.UserName,
+                    gender: formState.Gender,
+                    birthday: formState.Birthday,
+                    company: formState.Company,
+                    address1: formState.Address1,
+                    address2: formState.Address2,
+                    address3: formState.Address3,
+                    city: formState.City,
+                    county: formState.County,
+                    state: formState.State,
+                    zipCode: formState.ZipCode,
+                    country: formState.Country,
+                    emailAddress: formState.EmailAddress,
+                    phoneNumber: formState.PhoneNumber,
+                    eveningPhone: formState.EveningPhone,
+                    mobilePhone: formState.MobilePhone,
+                    fax: formState.Fax,
+                    notes: formState.Notes
+                })
             : axios.put(
-                `${baseUrl}/UpdatePassword`,
+                `${baseUrl}/UpdateAddress`,
                 {
                     Id: addressData.id,
                     categoryId: currentCategoryId!,
-                    addressName: formState.addressName,
-                    title: formState.title,
-                    firstName: formState.firstName,
-                    middleName: formState.middleName,
-                    lastName: formState.lastName,
-                    userName: formState.userName,
-                    gender: formState.gender,
-                    birthday: formState.birthday,
-                    company: formState.company,
-                    address1: formState.address1,
-                    address2: formState.address2,
-                    address3: formState.address3,
-                    city: formState.city,
-                    county: formState.county,
-                    state: formState.state,
-                    zipCode: formState.zipCode,
-                    country: formState.country,
-                    emailAddress: formState.emailAddress,
-                    phoneNumber: formState.phoneNumber,
-                    eveningPhone: formState.eveningPhone,
-                    mobilePhone: formState.mobilePhone,
-                    fax: formState.fax,
-                    notes: formState.notes
+                    addressName: formState.AddressName,
+                    title: formState.Title,
+                    firstName: formState.FirstName,
+                    middleName: formState.MiddleName,
+                    lastName: formState.LastName,
+                    userName: formState.UserName,
+                    gender: formState.Gender,
+                    birthday: formState.Birthday,
+                    company: formState.Company,
+                    address1: formState.Address1,
+                    address2: formState.Address2,
+                    address3: formState.Address3,
+                    city: formState.City,
+                    county: formState.County,
+                    state: formState.State,
+                    zipCode: formState.ZipCode,
+                    country: formState.Country,
+                    emailAddress: formState.EmailAddress,
+                    phoneNumber: formState.PhoneNumber,
+                    eveningPhone: formState.EveningPhone,
+                    mobilePhone: formState.MobilePhone,
+                    fax: formState.Fax,
+                    notes: formState.Notes
                 });
     }
 
@@ -142,43 +144,48 @@ const AddressCreationUpdateForm = ({ baseUrl, updateToggle, addressData }: { bas
 
     return (
         <form className={styles.CreateNewPasswordForm} onSubmit={handleFormSubmit} onClick={(e) => e.stopPropagation()}>
-            <span style={{ marginBottom: 50, fontWeight: 'bold', fontSize: 25 }}>Create New Password</span>
+            <span style={{ marginBottom: 50, fontWeight: 'bold', fontSize: 25 }}>Create New Address</span>
             <div className={styles.FormInputWrapperContainer}>
                 <div className={styles.FormInputWrapper}>
                     <span>Address Name</span>
-                    <input name="AddressName" id="AddressName" onChange={handleChange} className={styles.formInput} />
+                    <input type="text" name="AddressName" id="AddressName" onChange={handleChange} className={styles.formInput} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>Title</span>
-                    <input name="Title" id="Title" onChange={handleChange} className={styles.formInput} />
+                    <input type="text" name="Title" id="Title" onChange={handleChange} className={styles.formInput} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>First Name</span>
-                    <input name="FirstName" id="FirstName" onChange={handleChange} className={styles.formInput} />
+                    <input type="text" name="FirstName" id="FirstName" onChange={handleChange} className={styles.formInput} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>Middle Name</span>
-                    <input name="MiddleName" id="MiddleName" onChange={handleChange} className={styles.formInput} />
+                    <input type="text" name="MiddleName" id="MiddleName" onChange={handleChange} className={styles.formInput} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>Last Name</span>
-                    <input name="LastName" id="LastName" onChange={handleChange} className={styles.formInput} />
+                    <input type="text" name="LastName" id="LastName" onChange={handleChange} className={styles.formInput} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>User Name</span>
-                    <input name="UserName" id="UserName" onChange={handleChange} className={styles.formInput} />
+                    <input type="text" name="UserName" id="UserName" onChange={handleChange} className={styles.formInput} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>Gender</span>
-                    <input name="Gender" id="Gender" onChange={handleChange} className={styles.formInput} />
+                    <input type="text" name="Gender" id="Gender" onChange={handleChange} className={styles.formInput} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>Birthday</span>
-                    <input name="Birthday" id="Birthday" onChange={handleChange} className={styles.formInput} />
+                    <DatePicker selected={formState.Birthday} onChange={(date) => {
+                        setFormState({
+                            ...formState,
+                            Birthday: date?.toISOString()
+                        });
+                    }} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>Company</span>
-                    <input name="Company" id="Company" onChange={handleChange} className={styles.formInput} />
+                    <input type="text" name="Company" id="Company" onChange={handleChange} className={styles.formInput} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>Category</span>
@@ -186,59 +193,59 @@ const AddressCreationUpdateForm = ({ baseUrl, updateToggle, addressData }: { bas
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>Address 1</span>
-                    <input name="Address1" id="Address1" onChange={handleChange} className={styles.formInput} />
+                    <input type="text" name="Address1" id="Address1" onChange={handleChange} className={styles.formInput} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>Address 2</span>
-                    <input name="Address2" id="Address2" onChange={handleChange} className={styles.formInput} />
+                    <input type="text" name="Address2" id="Address2" onChange={handleChange} className={styles.formInput} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>Address 3</span>
-                    <input name="Address3" id="Address3" onChange={handleChange} className={styles.formInput} />
+                    <input type="text" name="Address3" id="Address3" onChange={handleChange} className={styles.formInput} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>City</span>
-                    <input name="City" id="City" onChange={handleChange} className={styles.formInput} />
+                    <input type="text" name="City" id="City" onChange={handleChange} className={styles.formInput} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>County</span>
-                    <input name="County" id="County" onChange={handleChange} className={styles.formInput} />
+                    <input type="text" name="County" id="County" onChange={handleChange} className={styles.formInput} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>State</span>
-                    <input name="State" id="State" onChange={handleChange} className={styles.formInput} />
+                    <input type="text" name="State" id="State" onChange={handleChange} className={styles.formInput} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>Zip Code</span>
-                    <input name="ZipCode" id="ZipCode" onChange={handleChange} className={styles.formInput} />
+                    <input type="number" name="ZipCode" id="ZipCode" onChange={handleChange} className={styles.formInput} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>Country</span>
-                    <input name="Country" id="Country" onChange={handleChange} className={styles.formInput} />
+                    <input type="text" name="Country" id="Country" onChange={handleChange} className={styles.formInput} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>Email</span>
-                    <input name="Email" id="Email" onChange={handleChange} className={styles.formInput} />
+                    <input type="email" name="EmailAddress" id="EmailAddress" onChange={handleChange} className={styles.formInput} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>Phone Number</span>
-                    <input name="PhoneNumber" id="PhoneNumber" onChange={handleChange} className={styles.formInput} />
+                    <input type="number" name="PhoneNumber" id="PhoneNumber" onChange={handleChange} className={styles.formInput} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>Evening Phone</span>
-                    <input name="EveningPhone" id="EveningPhone" onChange={handleChange} className={styles.formInput} />
+                    <input type="text" name="EveningPhone" id="EveningPhone" onChange={handleChange} className={styles.formInput} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>Mobile Phone</span>
-                    <input name="MobilePhone" id="MobilePhone" onChange={handleChange} className={styles.formInput} />
+                    <input type="number" name="MobilePhone" id="MobilePhone" onChange={handleChange} className={styles.formInput} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>Fax</span>
-                    <input name="Fax" id="Fax" onChange={handleChange} className={styles.formInput} />
+                    <input type="number" name="Fax" id="Fax" onChange={handleChange} className={styles.formInput} />
                 </div>
                 <div className={styles.FormInputWrapper}>
                     <span>Notes</span>
-                    <input name="Notes" id="Notes" onChange={handleChange} className={styles.formInput} />
+                    <input type="text" name="Notes" id="Notes" onChange={handleChange} className={styles.formInput} />
                 </div>
             </div>
             <div className={styles.BtnWrapper}>
