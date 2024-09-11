b69c0d87315bb28483aace8dffccc364a02cf4b3
rrich-kray <rrich.kray.93@gmail.com>
2024-09-10 18:34:30 -0700

    updated data modal

--- lastpass_clone.client/src/Components/DataForm/DataForm.tsx
+++ lastpass_clone.client/src/Components/DataForm/DataForm.tsx
@@ -1,21 +1,30 @@
 import styles from "./styles.module.scss";
+import globalStyles from "../../Global/CreationUpdateForm.module.scss";
 
 const DataForm = ({ data }: { data: object }) => {
+    console.log(data);
     const formatInputFieldTitle = (input: string): string =>
         input.split(/(?=[A-Z])/).map(word => word[0].toUpperCase() + word.slice(1)).join(" ");
     const rows: React.ReactNode[] = [];
     for (const key in data) {
         if (key != "id" && key != "categoryId") {
             rows.push(
-                <div className={styles.PasswordDataModalInfo}>
-                    <span>{formatInputFieldTitle(key)}</span>
-                    <span>{data[key]}</span>
-                </div>)
+                <tr>
+                    <td className={globalStyles.TableColumnOne}>{formatInputFieldTitle(key)}</td>
+                    <td className={globalStyles.TableColumnTwo}>
+                        <span>{data[key]}</span>
+                    </td>
+                </tr>)
         }
     }
     return (
-        <div className={styles.PasswordDataModal} onClick={(e) => e.stopPropagation()}>
-            {rows}
+        <div className={styles.DataModal} onClick={(e) => e.stopPropagation()}>
+            <div className={styles.DataModalHeader}></div>
+            <table className={globalStyles.CreateNewPasswordBodyRightPanelTable}>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
         </div>
     )
 }
