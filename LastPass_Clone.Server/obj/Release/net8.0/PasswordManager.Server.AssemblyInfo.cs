f2b869208ecdf3774a57b17cc650ea5bd33e3083
rrich-kray <rrich.kray.93@gmail.com>
2024-09-30 20:06:36 -0700

    pre-merge

--- LastPass_Clone.Server/obj/Release/net8.0/PasswordManager.Server.AssemblyInfo.cs
+++ LastPass_Clone.Server/obj/Release/net8.0/PasswordManager.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PasswordManager.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb2a58474983131b4a9a32e3eb67d3532c077c0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24b94cea8ae97133ca7a8decb99928b38894368a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PasswordManager.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PasswordManager.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
