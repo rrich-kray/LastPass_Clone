4f6dd25317fa49c3e4896dae61cb8e78d113159e
rrich-kray <rrich.kray.93@gmail.com>
2024-09-30 20:20:31 -0700

    new gitignore

--- LastPass_Clone.Server/obj/Release/net8.0/PasswordManager.Server.AssemblyInfo.cs
+++ LastPass_Clone.Server/obj/Release/net8.0/PasswordManager.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PasswordManager.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6de22d3b184e65642691ce9031f9c9e4736c0a56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a81f9ffdf7d1ba9ea954f3e7109c2cd372e8eb59")]
 [assembly: System.Reflection.AssemblyProductAttribute("PasswordManager.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PasswordManager.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
