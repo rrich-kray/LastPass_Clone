00be2272cfb9e8e33d56d24558a9be2074db3c8e
rrich-kray <rrich.kray.93@gmail.com>
2024-10-03 18:33:00 -0700

    ..

--- LastPass_Clone.Server/obj/Debug/net8.0/PasswordManager.Server.AssemblyInfo.cs
+++ LastPass_Clone.Server/obj/Debug/net8.0/PasswordManager.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PasswordManager.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94b0c02fc93d8626edc2affc26a4170aba6517c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c2b30f06aad5880aa42cbcaf29b04e0fc970520")]
 [assembly: System.Reflection.AssemblyProductAttribute("PasswordManager.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PasswordManager.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
